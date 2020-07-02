9399d2b3c585ebb3d6e9979534b999be21fb4c9f
Richard Lee <lee.richardm@gmail.com>
2020-07-02 07:16:25 -0700

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            string Name;
+            double HieghtInInches = 72.5;
+            string nameOfMyNewRockBand;
+            Name = "John";
+            nameOfMyNewRockBand = "The New Fiery Dvelopers";
+
+            Console.WriteLine("{0} is the number {1} member of {2}", Name, x, nameOfMyNewRockBand);
+            Console.WriteLine($"{Name} is the number {x} member of {nameOfMyNewRockBand}");
+            Console.Beep();
+        }
+    }
+}
