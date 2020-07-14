f411d04a8dc68ad969fc1dd3ad716863458df3d8
Richard Lee <lee.richardm@gmail.com>
2020-07-14 11:47:19 -0700

    After Greater Than Method

---
+++ GreaterThanInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreaterThanInput
+{
+    class Program
+    {
+        static string GreaterThan(int number1, int number2)
+        {
+            if (number1 > number2)
+            {
+                return $"{number1} is greater than {number2}";
+            }
+            else if(number2 > number1)
+            {
+                return $"{number2} is greater than {number1}";
+            }
+            else
+            {
+                return $"{number1} is equal to {number2}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GreaterThan(6,6));
+        }
+    }
+}
