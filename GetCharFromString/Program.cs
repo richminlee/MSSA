c6481446db902c0bece83949c87ef392fa77712c
Richard Lee <lee.richardm@gmail.com>
2020-07-14 10:38:07 -0700

    Whiteboarding

---
+++ GetCharFromString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GetCharFromString
+{
+    class Program
+    {
+		static int GetNumChar(string movieName, Char givenChar)
+		{
+			int count = 0;
+			givenChar = char.ToLower(givenChar);
+			movieName = movieName.ToLower();
+			for (int i = 0; i < movieName.Length; i++)
+			{
+				if (movieName[i] == givenChar)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetNumChar("moviename", 'e')); ;
+		}
+	}
+}
---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static string ReverseString(string word)
+        {
+            string reversedString = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedString += word[i];
+            }
+            return reversedString;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("Hello"));
+        }
+    }
+}
