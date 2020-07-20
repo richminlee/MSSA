16672f952427373a42d1a22783068c6cc05a32c1
Richard Lee <lee.richardm@gmail.com>
2020-07-20 08:41:34 -0700

    after week 3 homework

--- MathematicalFormulas/Program.cs
+++ MathematicalFormulas/Program.cs
@@ -35,24 +35,33 @@ namespace MathematicalFormulas
             int intradius = checkInput();
             double circumference = 2 * Math.PI * intradius;
             Console.WriteLine($"The circumference is {circumference}");
+            try
+            {
+                double area = Math.PI * (intradius * intradius);
+                Console.WriteLine($"The area is {area}");
 
-            double area = Math.PI * (intradius * intradius);
-
-            Console.WriteLine($"The area is {area}");
+            }
+            catch (OverflowException oEx)
+            {
+                Console.WriteLine(oEx.Message);
+            }
 
             // Part 2
             Console.WriteLine("\nPart 2, volume of a hemisphere.");
+            Console.Write("Enter an integer for the radius: ");
+            intradius = checkInput();
             float fraction = 4 / 3f;
             double volume = (fraction * Math.PI * (Math.Pow(intradius, 3))) / 2;
-                
-
-
             Console.WriteLine($"The volume is {volume}");
-
 
             // Part 3
             Console.WriteLine("\nPart 3, area of a triangle (Heron's formula).");
-
+            Console.Write("Enter an integer for the first side of the triangle: ");
+            double firstInput = checkInput();
+            Console.Write("Enter an integer for the second side of the triangle: ");
+            double secondInput = checkInput();
+            Console.Write("Enter an integer for the third side of the triangle: ");
+            double thirdInput = checkInput();
             static double AreaTriangle(double a, double b, double c)
             {
                 double p = (a + b + c) / 2;
@@ -60,13 +69,16 @@ namespace MathematicalFormulas
                 return result;
 
             }
-
-            Console.WriteLine($"The area is {AreaTriangle(10.0, 10.0, 10.0)}");
-
+            Console.WriteLine($"The area is {AreaTriangle(firstInput, secondInput, thirdInput)}");
 
             // Part 4
             Console.WriteLine("\nPart 4, solving a quadratic equation.");
-
+            Console.Write("Enter an integer for the first part of the quadratic formula: ");
+            firstInput = checkInput();
+            Console.Write("Enter an integer for the second part of the quadratic formula: ");
+            secondInput = checkInput();
+            Console.Write("Enter an integer for the third part of the quadratic formula: ");
+            thirdInput = checkInput();
             static string Quadratic(double a, double b, double c)
             {
                 double denominator = 2 * a;
@@ -86,9 +98,16 @@ namespace MathematicalFormulas
                     return errorMessage;
                 }
                 return $"The positive solution is {positive_num} \r The negative solution is {negative_num}";
+            }
+            Console.WriteLine(Quadratic(firstInput, secondInput, thirdInput));
+            try
+            {
 
             }
-            Console.WriteLine(Quadratic(1,2,3));
+            finally
+            {
+                Console.WriteLine("This program has finally terminated.");
+            }
         }
     }
 }
