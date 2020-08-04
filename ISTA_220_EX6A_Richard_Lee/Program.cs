198fa6e05fc3408a6824b48c1072091c940d06bb
Richard Lee <lee.richardm@gmail.com>
2020-08-04 08:07:33 -0700

    After HowMuchMoney

---
+++ ISTA_220_EX6A_Richard_Lee/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace ISTA_220_EX6A_Richard_Lee
+{
+    class Program
+    {
+        static double AverageArray(int[] inputArray)
+        {
+            double count = 0;
+            double total = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                count++;
+                total += inputArray[i];
+            }
+            return total / count;
+        }
+
+        static int[] ReverseArray(int[] inputArray)
+        {
+            int[] reversedArray = new int[inputArray.Length];
+            int j = 0;
+            for(int i = inputArray.Length-1; i >= 0; i--)
+            {
+                reversedArray[j] = inputArray[i];
+                j++;
+            }
+            return reversedArray;
+        }
+
+        static int[] RotateArray(string direction, int numberOfPlaces, int[] inputArray)
+        {
+            int[] rotatedArray = new int[inputArray.Length];
+            int index = 0;
+            if(direction == "left")
+            {
+                for (int i = inputArray.Length - numberOfPlaces; i < inputArray.Length; i++)
+                {
+                    rotatedArray[index] = inputArray[i];
+                    index++;
+                }
+                for (int i = 0; i < inputArray.Length - numberOfPlaces; i++)
+                {
+                    rotatedArray[index] = inputArray[i];
+                    index++;
+                }
+            }
+            else // direction == "right"
+            {
+                for (int i = numberOfPlaces; i < inputArray.Length; i++)
+                {
+                    rotatedArray[index] = inputArray[i];
+                    index++;
+                }
+                for (int i = 0; i < numberOfPlaces; i++)
+                {
+                    rotatedArray[index] = inputArray[i];
+                    index++;
+                }
+            }
+            return rotatedArray;
+        }
+
+        static int[] BubbleSort(int[] inputArray)
+        {
+            int temp;
+            int count = 0;
+
+            for(int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if(inputArray[i] > inputArray[i + 1])
+                {
+                    temp = inputArray[i];
+                    inputArray[i] = inputArray[i + 1];
+                    inputArray[i + 1] = temp;
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                BubbleSort(inputArray);
+            }
+            return inputArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arrayA = new int[6] { 0, 2, 4, 6, 8, 10 };
+            int[] arrayB = new int[5] { 1, 3, 5, 7, 9 };
+            int[] arrayC = new int[12] { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+
+            // 1. 
+            // Console.WriteLine(AverageArray(arrayA));
+
+            // 2.
+            //int[] resultReverseArray = ReverseArray(arrayA); // uncomment these one at a time to check the input results.
+            //int[] resultReverseArray = ReverseArray(arrayB); // uncomment these one at a time to check the input results.
+            //int[] resultReverseArray = ReverseArray(arrayC); // uncomment these one at a time to check the input results.
+
+            //for (int i = 0; i < resultReverseArray.Length; i++)
+            //{
+            //    Console.Write($"{resultReverseArray[i]}, ");
+
+            //}
+
+            // 3.
+            //int[] resultRotatedArray = RotateArray("left", 2, arrayA); // uncomment these one at a time to check the input results.
+            //int[] resultRotatedArray = RotateArray("right", 2, arrayB); // uncomment these one at a time to check the input results.
+            //int[] resultRotatedArray = RotateArray("left", 4, arrayC); // uncomment these one at a time to check the input results.
+            //for (int i = 0; i < resultRotatedArray.Length; i++)
+            //{
+            //    Console.Write($"{resultRotatedArray[i]}, ");
+
+            //}
+
+            // 4. arrayC's arrangement of int is similar to Pi.
+            //int[] resultSortedArray = BubbleSort(arrayC);
+            //for (int i = 0; i < resultSortedArray.Length; i++)
+            //{
+            //    Console.Write($"{resultSortedArray[i]}, ");
+
+            //}
+        }
+    }
+}
---
+++ ISTA_220_EX6B_Richard_Lee/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ISTA_220_EX6B_Richard_Lee
+{
+    public struct Coordinates
+    {
+        public double x;
+        public double y;
+        public Coordinates(Random rand)
+        {
+            this.x = rand.NextDouble();
+            this.y = rand.NextDouble();
+        }
+        public Coordinates(double X, double Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+    }
+    class Program
+    {
+        static double findHypotenuse(double x, double y) => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        static void Main(string[] args)
+        {
+            double count = 0.0;
+            Random rand = new Random();
+            Coordinates[] numInput = new Coordinates[int.Parse(Console.ReadLine())];
+            for(int i = 0; i < numInput.Length; i++)
+            {
+                numInput[i] = new Coordinates(rand);
+                double hypotenuse = findHypotenuse(numInput[i].x, numInput[i].y);
+                if (hypotenuse <= 1) count++;
+            }
+            double result = (count / numInput.Length) * 4;
+            Console.WriteLine($"The calculated pi is {result}");
+            double difference = Math.Abs(result - Math.PI);
+            Console.WriteLine($"The difference between pi and the caluculated pi is {difference}");
+        }
+    }
+    //1. We are working in the Top right quadrant, so we need to multiply by 4 to get the whole circle.
+    //2. The output gets closer to Pi, becoming more accurate.
+    //3. No, the output changes because the inputs are random.
+    //4. 100,000,000 took a while to run and very accurate.
+    //5. Another way to use the Monte-Carlo Method is for probablilities. Given a larger number of samples, the average, minimum and maximum will form a bell curve.
+}
