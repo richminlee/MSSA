6bf2d7e0ca62484774ac2e05eb4779a523e6c331
Richard Lee <lee.richardm@gmail.com>
2020-08-10 22:02:12 -0700

    added classes

---
+++ ISTA_322_EX7A_Richard_Lee/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISTA_322_EX7A_Richard_Lee
+{
+    class Vehicle
+    {
+    }
+}
---
+++ ISTA_322_EX7A_Richard_Lee/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISTA_322_EX7A_Richard_Lee
+{
+    class Weapon
+    {
+    }
+    class SmallCaliberWeapon : Weapon
+    {
+
+    }
+    class IndirectFireWeapon : Weapon
+    {
+
+    }
+    class DirectFireWeapon : Weapon
+    {
+
+    }
+}
