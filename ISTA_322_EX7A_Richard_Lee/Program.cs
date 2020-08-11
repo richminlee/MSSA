e77ccc740512a3de7de7e8e303e72d7a854efc59
Richard Lee <lee.richardm@gmail.com>
2020-08-10 21:56:56 -0700

    added asp.net

---
+++ ISTA_322_EX7A_Richard_Lee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISTA_322_EX7A_Richard_Lee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
