be7252b5fb005601795ccbb628dd386b1521543c
Richard Lee <lee.richardm@gmail.com>
2020-08-13 12:59:22 -0700

    After Angle of Clock

---
+++ AngleOfClock/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AngleOfClock
+{
+    class Program
+    {
+        static double CalculateAngle(double hour, double minute)
+        {
+            double resultAngle = (Math.Abs(hour * 30 - minute * 6)) + (0.5 * minute);
+            if (resultAngle > 180) resultAngle = 360 - resultAngle;
+            return resultAngle;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateAngle(6,30));
+            Console.WriteLine(CalculateAngle(12,20));
+        }
+    }
+}
