d99f14ab56cea2ecc9fc4af850504a0662d8f00b
Richard Lee <lee.richardm@gmail.com>
2020-08-27 22:36:32 -0700

    integer swap

---
+++ IntegerSwap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntegerSwap
+{
+    class Program
+    {
+        public static void IntSwap()
+        {
+            int x = 5;
+            int y = 17;
+            Console.WriteLine($"x = {x}, y = {y}");
+            x += y;//22
+            y = x - y;//5
+            x = x - y;// 17
+            Console.WriteLine("Swap");
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+        static void Main(string[] args)
+        {
+            IntSwap();
+        }
+    }
+}
