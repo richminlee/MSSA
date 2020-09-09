ae2087a446acaf877deccf0062ef3d727402c311
Richard Lee <lee.richardm@gmail.com>
2020-09-08 20:58:22 -0700

    started BaseNumberConversion

---
+++ BaseNumberConversion/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Specialized;
+
+namespace BaseNumberConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the integer to convert: ");
+            string n1 = Console.ReadLine();
+            int number = int.Parse(n1);
+
+            Console.WriteLine("Please enter the base to convert from [2 | 8 | 10]: ");
+            string n2 = Console.ReadLine();
+            int from = int.Parse(n2);
+
+            Console.WriteLine($"Number: {number}, base: {from}");
+            int result = 0;
+            if(from == 10)
+            {
+                result = Util.dec2bin(number);
+                Console.WriteLine($"binary conversion is {result}");
+                result = Util.dec2oct(number);
+                Console.WriteLine($"octal conversion is {result}");
+            }            
+            else if(from == 2)
+            {
+                result = Util.bin2dec(number);
+                Console.WriteLine($"decimal conversion is {result}");
+                result = Util.bin2oct(number);
+                Console.WriteLine($"octal conversion is {result}");
+            }
+            else if(from == 8)
+            {
+                result = Util.oct2bin(number);
+                Console.WriteLine($"binary conversion is {result}");
+                result = Util.oct2dec(number);
+                Console.WriteLine($"decimal conversion is {result}");
+            }
+            else
+            {
+                Console.WriteLine("Error in base to convert from");
+            }
+
+        }
+    }
+}
---
+++ BaseNumberConversion/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseNumberConversion
+{
+    class Util
+    {
+        public static int dec2bin(int input)
+        {
+
+        }
+        public static int dec2oct(int input)
+        {
+
+        }
+        public static int bin2dec(int input)
+        {
+
+        }
+        public static int bin2oct(int input)
+        {
+
+        }
+        public static int oct2bin(int input)
+        {
+
+        }
+        public static int oct2dec(int input)
+        {
+
+        }
+    }
+}
