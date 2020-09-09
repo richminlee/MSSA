eea7d76ded5529a707cfa98bd9b4759e32bc6282
Richard Lee <lee.richardm@gmail.com>
2020-09-08 21:41:09 -0700

    added struct

--- KeyValueStore/Program.cs
+++ KeyValueStore/Program.cs
@@ -2,11 +2,37 @@
 
 namespace KeyValueStore
 {
+    struct KeyValue
+    {
+        public readonly string Key;
+        public readonly object Value;
+        public KeyValue(string key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+    class MyDictionary
+    {
+        public KeyValue[] 
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
         }
     }
 }
