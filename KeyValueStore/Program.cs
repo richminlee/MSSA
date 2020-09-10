27897991eff7163f965c2183867307aaf2011836
Richard Lee <lee.richardm@gmail.com>
2020-09-10 11:54:22 -0700

    Finished Contoso assignment

--- KeyValueStore/Program.cs
+++ KeyValueStore/Program.cs
@@ -4,17 +4,23 @@ namespace KeyValueStore
 {
     struct KeyValue
     {
-        public readonly string Key;
-        public readonly object Value;
+        public readonly string _Key;
+        public readonly object _Value;
         public KeyValue(string key, object value)
         {
-            this.Key = key;
-            this.Value = value;
+            this._Key = key;
+            this._Value = value;
         }
     }
     class MyDictionary
     {
-        public KeyValue[] 
+        private string _key;
+        private object _value;
+        public MyDictionary(KeyValue input)
+        {
+            this._key = input._Key;
+            this._value = input._Value;
+        }
     }
 
     class Program
