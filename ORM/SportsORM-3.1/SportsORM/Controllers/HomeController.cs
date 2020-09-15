003879db6714c4920ee1fa7dfde447e062c5f906
Richard Lee <lee.richardm@gmail.com>
2020-09-14 23:26:15 -0700

    Added Sports ORM

---
+++ ORM/SportsORM-3.1/SportsORM/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsORM.Models;
+
+
+namespace SportsORM.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static Context _context;
+
+        public HomeController(Context DBContext)
+        {
+            _context = DBContext;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            ViewBag.BaseballLeagues = _context.Leagues
+                .Where(l => l.Sport.Contains("Baseball"))
+                .ToList();
+            return View();
+        }
+
+        [HttpGet("level_1")]
+        public IActionResult Level1()
+        {
+            ViewBag.WomenLeagues = _context.Leagues
+                .Where(l => l.Name.Contains("Women"))
+                .ToList();
+            ViewBag.HockeyLeagues = _context.Leagues
+                .Where(l => l.Sport.Contains("Hockey"))
+                .ToList();
+            ViewBag.NotFootballLeagues = _context.Leagues
+                .Where(l => !l.Sport.Contains("Football"))
+                .ToList();
+            ViewBag.ConferenceLeagues = _context.Leagues
+                .Where(l => l.Name.Contains("Conference"))
+                .ToList();
+            ViewBag.AtlanticLeagues = _context.Leagues
+                .Where(l => l.Name.Contains("Atlantic"))
+                .ToList();
+            ViewBag.DallasTeams = _context.Teams
+                .Where(l => l.Location.Contains("Dallas"))
+                .ToList();
+            ViewBag.RaptorsTeams = _context.Teams
+                .Where(l => l.TeamName.Contains("Raptors"))
+                .ToList();
+            return View();
+        }
+
+        [HttpGet("level_2")]
+        public IActionResult Level2()
+        {
+            return View();
+        }
+
+        [HttpGet("level_3")]
+        public IActionResult Level3()
+        {
+            return View();
+        }
+
+    }
+}
