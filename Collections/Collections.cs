9c7f61fdc7b850fef89ca4a4a1beea3af0e6b9dd
Richard Lee <lee.richardm@gmail.com>
2020-09-22 09:50:48 -0700

    added some collections practice

---
+++ Collections/Collections.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class Collections
+    {
+        public static int[] GetScores(int numOfScores)
+        {
+            Random random = new Random();
+            int[] result = new int[numOfScores];
+            for (int i = 0; i < numOfScores; i++)
+            {
+                result[i] = random.Next(1, 100);
+            }
+            return result;
+        }
+        public static double AverageScores(int[] arrOfScores)
+        {
+            double result = 0;
+            for (int i = 0; i < arrOfScores.Length; i++)
+            {
+                result += arrOfScores[i];
+            }
+            result /= arrOfScores.Length;
+            return result /= arrOfScores.Length;
+        }
+        public static int MaximumValue(int[] arrOfScores)
+        {
+            int maxValue = arrOfScores[0];
+            for (int i = 0; i < arrOfScores.Length; i++)
+            {
+                if (maxValue < arrOfScores[i])
+                {
+                    maxValue = arrOfScores[i];
+                }
+            }
+            return maxValue;
+        }
+        public static int[] SortArray(int[] arrOfScores)
+        {
+            int idx1 = 0;
+            int idx2 = 1;
+            bool toggle = false;
+            while (idx2 < arrOfScores.Length)
+            {
+                if(arrOfScores[idx1] > arrOfScores[idx2])
+                {
+                    int temp = arrOfScores[idx1];
+                    arrOfScores[idx1] = arrOfScores[idx2];
+                    arrOfScores[idx2] = temp;
+                    toggle = true;
+                }
+                idx1++;
+                idx2++;
+            }
+            if(toggle == true)
+            {
+                return SortArray(arrOfScores);
+            }
+            return arrOfScores;
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = Collections.SortArray(new int[] { 5, 3, 7, 1, 2 });
+            foreach (int num in result)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
