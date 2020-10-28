b60be1d7b3403c72bdc5c4c0a5b1d457f3efaf44
Richard Lee <lee.richardm@gmail.com>
2020-10-28 11:45:41 -0700

    added camelcase

---
+++ camelCase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace camelCase
+{
+    class Program
+    {
+        public static string MakePascalCase(string[] input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string word = input[i].ToLower();
+                StringBuilder modWord = new StringBuilder();
+                bool nextCap = true;
+
+                for (int j = 0; j < word.Length; j++)
+                {
+
+
+                    if (word[j] == ' ') nextCap = true;
+
+                    else if (word[j] >= 'a' && word[j] <= 'z' && nextCap == true)
+                    {
+                        modWord.Append(Char.ToUpper(word[j]));
+                        nextCap = false;
+                    }
+                    else if (word[j] >= 'a' && word[j] <= 'z') modWord.Append(word[j]);
+                }
+
+                result.Append(modWord.ToString());
+                modWord.Clear();
+            }
+
+            return result.ToString();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakePascalCase(new string[] { "hello 2-C!ody", "hOW", "aRe", "YOU"}));
+        }
+    }
+}
